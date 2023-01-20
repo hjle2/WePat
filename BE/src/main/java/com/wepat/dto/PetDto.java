9718ff80d2d0226afd0ab027ba2828c3ace7b986
최현제 <qwas15788@gmail.com>
2023-01-20 13:10:16 +0900

    [Update] gitignore

--- BE/src/main/java/com/wepat/dto/PetDto.java
+++ BE/src/main/java/com/wepat/dto/PetDto.java
@@ -18,6 +18,8 @@ public class PetDto {
     private int age;
     @ApiParam(value = "펫 프로필")
     private String photoURL;
+    @ApiParam(value = "프로필 색상")
+    private String color;
     @ApiParam(value = "반려동물 생일")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthday;
