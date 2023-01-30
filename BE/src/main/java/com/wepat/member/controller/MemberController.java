2b4c37d24bd288d938ba5f035c331e73bcaa6f50
hjle2 <02q09q@naver.com>
2023-01-30 17:18:34 +0900

    merge

--- BE/src/main/java/com/wepat/member/controller/MemberController.java
+++ BE/src/main/java/com/wepat/member/controller/MemberController.java
@@ -27,6 +27,36 @@ public class    MemberController {
     private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
     private final MemberService memberService;
     private final JwtUtil jwtUtil;
+    @PostMapping("/signin")
+    @ApiOperation(value = "로그인 시도",  notes = "로그인 요청을 한다.",response = MemberDto.class)
+    public ResponseEntity<?> signIn(String memberId, String pwd) {
+        try {
+            Map<String, String> resultMap = new HashMap<>();
+            MemberDto memberResult = memberService.signIn(memberId, pwd);//유저가 로그인 가능한 유저인지 확인
+            String accessToken = null;
+            String refreshToken = null;//유저가 로그인 되면 토큰을 생성하여 저장할 String
+            if(memberResult != null){//로그인에서 객체를 받아왔다.
+                accessToken = jwtUtil.createAccessToken("memberId", memberId);
+                refreshToken = jwtUtil.createRefreshToken("memberId", memberId);
+                memberService.saveRefreshToken(memberId, refreshToken);
+
+                resultMap.put("access-token", accessToken);
+                resultMap.put("refresh-token", refreshToken);
+                return new ResponseEntity<>(resultMap, HttpStatus.ACCEPTED);
+            } else {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        } catch (IdWriteException e) {
+            throw new IdWriteException(e.getMessage());
+        } catch (BlockMember e) {
+            throw new BlockMember(e.getMessage());
+        } catch (PwdWriteException e) {
+            throw new PwdWriteException(e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
     @PostMapping("/signup")
     @ApiOperation(value = "회원가입", notes = "정보를 받아 회원가입 시도한다.", response = MemberDto.class)
     public ResponseEntity<?> signUp(MemberDto member) {
