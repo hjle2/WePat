e5379e03f6ce3901e1b3e65e407cd1e72edc465c
hjle <02q09q@naver.com>
2023-02-01 05:07:35 +0900

    ec2 test

--- BE/src/main/java/com/wepat/config/FirebaseConfig.java
+++ BE/src/main/java/com/wepat/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
@@ -13,10 +14,13 @@ public class FirebaseConfig  {
     @PostConstruct
     public void init(){
         try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            ClassPathResource serviceAccount = new ClassPathResource("serviceAccountKey.json");
+//            FileInputStream serviceAccount =
+//                    new FileInputStream("src/main/resources/serviceAccountKey.json");
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                    .setDatabaseUrl("AIzaSyDBscWBxZ3rgirNFIu2HYByRSLX2gsrn2A")
                     .build();
             FirebaseApp.initializeApp(options);
         }catch (Exception e){
--- BE/src/main/java/com/wepat/exception/ApiExceptionHandler.java
+++ BE/src/main/java/com/wepat/exception/ApiExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.wepat.exception;
 
-import com.wepat.gcp.FileWriteException;
-import com.wepat.gcp.GCPFileUploadException;
-import com.wepat.gcp.InvalidFileTypeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,17 +10,6 @@ import java.time.ZonedDateTime;
 @ControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler(InvalidFileTypeException.class)
-    @ResponseBody
-    public ApiException handleInvalidFileException(InvalidFileTypeException e) {
-        ApiException exception = new ApiException();
-        exception.setErrorMessage(e.toString());
-        exception.setStatusCode(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
-        exception.setZonedDateTime(ZonedDateTime.now());
-        return exception;
-    }
-
-    @ExceptionHandler(BadRequestException.class)
     @ResponseBody
     public ApiException handleBadRequestException(BadRequestException e){
         ApiException exception = new ApiException();
@@ -33,25 +19,6 @@ public class ApiExceptionHandler {
         return exception;
     }
 
-    @ExceptionHandler(GCPFileUploadException.class)
-    @ResponseBody
-    public ApiException handleFileUploadException(GCPFileUploadException e){
-        ApiException exception = new ApiException();
-        exception.setErrorMessage(e.getMessage());
-        exception.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        exception.setZonedDateTime(ZonedDateTime.now());
-        return exception;
-    }
-
-    @ExceptionHandler(FileWriteException.class)
-    @ResponseBody
-    public ApiException handleFileWriteException(FileWriteException e){
-        ApiException exception = new ApiException();
-        exception.setErrorMessage(e.getMessage());
-        exception.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        exception.setZonedDateTime(ZonedDateTime.now());
-        return exception;
-    }
     @ExceptionHandler(UnAuthorizedException.class)
     @ResponseBody
     public ApiException handleUnAuthorizedException(UnAuthorizedException e){
