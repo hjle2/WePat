644cfb99f05a239a54b0229a37ea9333fb64fbfa
hjle2 <02q09q@naver.com>
2023-02-01 16:29:07 +0900

    test

--- BE/src/main/java/com/wepat/photo/service/PhotoService.java
+++ BE/src/main/java/com/wepat/photo/service/PhotoService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public interface PhotoService {
-    // 전체 앨범 가져오기
+    // 전체 앨범 가져오기 //
     List<PhotoDto> getAllPhotoById(String calendarId) throws ExecutionException, InterruptedException ;
     // 특정 사진 선택 시 보여주기
     PhotoDto getPhotoById(String photoId) throws ExecutionException, InterruptedException ;
