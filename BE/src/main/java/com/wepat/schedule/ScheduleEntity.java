d69a75cffabb9efe35dc28c04424509a6254306f
hjle2 <02q09q@naver.com>
2023-02-03 12:30:25 +0900

    scheduledto

--- BE/src/main/java/com/wepat/schedule/ScheduleEntity.java
+++ BE/src/main/java/com/wepat/schedule/ScheduleEntity.java
@@ -8,36 +8,37 @@ import java.util.List;
 
 @Data
 public class ScheduleEntity {
-    @ApiParam(value = "스케쥴ID", required = true)
+    private String calendarId;
+    @ApiParam(value = "스케쥴 id", hidden = true)
     private String scheduleId;
     @ApiParam(value = "스케쥴 이름")
     private String title;
     @ApiParam(value = "카테고리")
     private String category;
     @ApiParam(value = "달력에 표시 여부")
-    private boolean diplay;
+    private boolean display;
     @ApiParam(value = "펫ID(PK)", required = true)
     private String petId;
-    @ApiParam(value = "반복여부")
-    private boolean repeat = false;
     @ApiParam(value = "일정 시작일")
     private String startDate;
     @ApiParam(value = "일정 종료일")
     private String endDate;
-    @ApiParam(value = "일정 작성일")
-    private String date;
+    @ApiParam(value = "알람 여부")
+    private int alarm;
+    @ApiParam(value = "일정 예정자")
+    private String whoPlanned;
+    @ApiParam(value = "일정 완료자")
+    private String whoCompleted;
     @ApiParam(value = "일정 수행 여부")
     private boolean completed = false;
     @ApiParam(value = "반복 주기")
-    private int unit;
+    private int repeatUnit;
     @ApiParam(value = "일 or 달")
-    private int unitSize;
-    @ApiParam(value = "몸무게")
-    private double weight;
+    private int repeatAmount;
     @ApiParam(value = "일정에 관한 메모")
     private String memo;
-    @ApiParam(value = "사진ID 리스트")
+    @ApiParam(value = "사진ID 리스트", hidden = true)
     private List<String> photoList;
-    @ApiParam(value = "댓글Dto 리스트")
+    @ApiParam(value = "댓글Dto 리스트", hidden = true)
     private List<CommentDto> reviewList;
 }
