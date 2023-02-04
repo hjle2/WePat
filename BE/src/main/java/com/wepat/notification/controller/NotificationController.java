672ea5046d1ac7218682f876d97a37aaf82a67e7
hjle <02q09q@naver.com>
2023-02-05 01:19:03 +0900

    url bug

--- BE/src/main/java/com/wepat/notification/controller/NotificationController.java
+++ BE/src/main/java/com/wepat/notification/controller/NotificationController.java
@@ -1,11 +1,12 @@
 package com.wepat.notification.controller;
 
-import com.wepat.notification.NotificationDto;
 import com.wepat.notification.service.NotificationService;
+import com.wepat.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.InvalidParameterException;
 import java.util.concurrent.ExecutionException;
 
@@ -26,14 +27,16 @@ public class NotificationController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/deleteAll/{memberId}")
-    public ResponseEntity<?> deleteAll(@PathVariable("memberId") String memberId) {
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<?> deleteAll(HttpServletRequest request) {
+        String memberId = JwtUtil.getUserIdByHttpRequest(request);
         notificationService.deleteAll(memberId);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/readAll/{memberid}")
-    public ResponseEntity<?> readlAll(@PathVariable("memberId") String memberId) {
+    @DeleteMapping("/readAll")
+    public ResponseEntity<?> readlAll(HttpServletRequest request) {
+        String memberId = JwtUtil.getUserIdByHttpRequest(request);
         notificationService.readlAll(memberId);
         return ResponseEntity.ok().build();
     }
@@ -42,6 +45,19 @@ public class NotificationController {
     public ResponseEntity<?> getScheduleIdByNotificationId(@PathVariable("notificationid") String notificationId) {
         try {
             notificationService.getScheduleIdByNotificationId(notificationId);
+        } catch (ExecutionException e) {
+            throw new InvalidParameterException();
+        } catch (InterruptedException e) {
+            throw new InvalidParameterException();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<?> getScheduleIdByNotificationId(HttpServletRequest request) {
+        try {
+            String memberId = JwtUtil.getUserIdByHttpRequest(request);
+            notificationService.getScheduleIdByNotificationId(memberId);
         } catch (ExecutionException e) {
             throw new InvalidParameterException();
         } catch (InterruptedException e) {
