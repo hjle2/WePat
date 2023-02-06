52a3285c612b60c9c4cfcd55aa2d73cdc2bacb6b
hjle2 <02q09q@naver.com>
2023-02-06 17:19:59 +0900

    firebase key change

--- BE/src/main/java/com/wepat/config/FirebaseConfig.java
+++ BE/src/main/java/com/wepat/config/FirebaseConfig.java
@@ -14,7 +14,7 @@ public class FirebaseConfig  {
     @PostConstruct
     public void init(){
         try{
-            ClassPathResource serviceAccount = new ClassPathResource("firebase_key.json");
+            ClassPathResource serviceAccount = new ClassPathResource("serviceAccountKey.json");
 //            FileInputStream serviceAccount =
 //                    new FileInputStream("src/main/resources/serviceAccountKey.json");
             FirebaseOptions options = new FirebaseOptions.Builder()
